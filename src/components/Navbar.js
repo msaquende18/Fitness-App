052c9a049a2ff8324502ce2dc71564f0d7076095
Marcelino Saquende <102360616+msaquende18@users.noreply.github.com>
2024-12-05 23:19:04 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => (
       alignItems="flex-end"
     >
       <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
-      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercícios</a>
     </Stack>
   </Stack>
 );
