bf6351794c7b1f800606424696b761037ab882e1
Marcelino Saquende <102360616+msaquende18@users.noreply.github.com>
2024-12-05 23:17:03 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
       <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Feito com ❤️ por Lueji Technologies</Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Feito com ❤️ Por Lueji Technologies</Typography>
   </Box>
 );
 
